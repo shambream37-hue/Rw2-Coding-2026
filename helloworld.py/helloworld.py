56273a14cd965ee03a104add6af78778c8983f0f
HighVibeTechGirl <shambream37@gmail.com>
2026-02-13 03:47:02 +0000

    Created helloworld

---
+++ helloworld.py/helloworld.py
@@ -0,0 +1,13 @@
+
+# the hash is a one line comment, nothing here execute
+
+"""
+the triple quotes are a multi-line comment
+see
+believe me
+anything inside will not execute
+""'
+
+#
+ this will print the screan
+ print("hello world!")
