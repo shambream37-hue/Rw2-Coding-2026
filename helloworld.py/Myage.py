9340f0aad724802da60b3e13a7bede6eb9eda077
HighVibeTechGirl <shambream37@gmail.com>
2026-02-18 03:54:37 +0000

    We are getting pythonic

---
+++ helloworld.py/Myage.py
@@ -0,0 +1,6 @@
+birth_year = 1989
+c_year = 2026 
+age = c_year - birth_year
+name = "Brea" 
+# print to the screen:
+print("my name is" + name + "and my age is" + str(age))
--- helloworld.py/helloworld.py
+++ helloworld.py/helloworld.py
@@ -5,9 +5,27 @@
 the triple quotes are a multi-line comment
 see
 believe me
-anything inside will not execute
-""'
+anything
+"""
 
-#
- this will print the screan
- print("hello world!")
+# some other data types
+
+price = 5.99
+age = 35
+is_here = True
+# string
+name = "Brea" 
+
+# this the python if statenet
+if age > 36:
+    print("your a autn")
+
+elif":"
+    print("you are not a aunt")
+else:
+    print("you must be 35 yaers old")
+
+
+    # python operators arithmetic and comparison
+    "" "" ""
+    
